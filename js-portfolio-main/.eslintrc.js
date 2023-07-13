d08585779007d772aea5b221da7173c530ef195c
hfjuarez <herfj4@gmail.com>
2023-07-12 21:19:28 -0300

    add eslint, optimization, mini css

---
+++ js-portfolio-main/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "sort-keys": [
+      "error",
+      "asc",
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
+  },
+};
--- js-portfolio-main/webpack.config.js
+++ js-portfolio-main/webpack.config.js
@@ -1,50 +1,66 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  resolve: {
-    extensions: [".js"],
-  },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
         exclude: /node_modules/,
+        test: /\.m?js$/,
         use: {
           loader: "babel-loader",
         },
       },
       {
-        test: /\.css$/i,
-        use: [MiniCSSExtractPlugin.loader, "css-loader"],
+        test: /.s?css$/,
+        use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.png/,
-        type: 'asset/resource'
-      }
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            esModule: false,
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name][contenthash].[ext]",
+            outputPath: "./public/fonts/",
+            publicPath: "./public/fonts/",
+          },
+        },
+      },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  output: {
+    assetModuleFilename: "public/images/[hash][ext][query]",
+    clean: true,
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
+      filename: "./index.html",
       inject: true,
       template: "./public/index.html",
-      filename: "./index.html",
     }),
-    new MiniCSSExtractPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "src", "assets/images"),
-          to: "assets/images",
-        },
-      ],
+    new MiniCSSExtractPlugin({
+      filename: "public/[name].[contenthash].css",
     }),
+    new CssMinimizerPlugin(),
   ],
+  resolve: {
+    extensions: [".js"],
+  },
 };
